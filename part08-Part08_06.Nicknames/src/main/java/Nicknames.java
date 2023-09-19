87dde4946654b54780529cfcaa8dae67a41febba
Jorge Nogueira <jorgetazem@hotmail.com>
2023-09-19 15:32:44 +0100

    Part 8 - Ex 6

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,16 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        
+        HashMap<String,String> storeNames = new HashMap<>();
+        
+        storeNames.put("matthew","matt");
+        storeNames.put("michael","mix");
+        storeNames.put("arthur","artie");
+        
+        System.out.println(storeNames.get("matthew"));
+        
+        
     }
 
 }
